9654842411e73d973add3efb9a82592a4e167992
devjangir <devpassion7@gmail.com>
2024-05-08 13:44:53 +0530

    Initial Commit

---
+++ UIBasics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UIBasics
+//
+//  Created by djangir on 08/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UIBasics/UIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIBasicsApp.swift
+//  UIBasics
+//
+//  Created by djangir on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
