fc2c8986eeebdf029792c4e565c341df7f42fdbd
devjangir <devpassion7@gmail.com>
2024-05-09 14:42:32 +0530

    Different Basic View Created

---
+++ UIBasics/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  UIBasics
+//
+//  Created by djangir on 08/05/24.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
---
+++ UIBasics/UserListView.swift
@@ -0,0 +1,18 @@
+//
+//  UserListView.swift
+//  UIBasics
+//
+//  Created by djangir on 09/05/24.
+//
+
+import SwiftUI
+
+struct UserListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserListView()
+}
